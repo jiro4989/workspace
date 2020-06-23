0238d16bbe2a919a24a81b8e8bf558b1e0d7689f
jiro4989 <jiroron666@gmail.com>
2020-06-23 09:48:40 +0900

    Ubuntu20.04で動かなかったけれど動くようになった・・・？

--- recreate_base.sh
+++ recreate_base.sh
@@ -2,15 +2,22 @@
 
 set -eux
 
-for d in base base-gui; do
-  (
-    cd "$d"
-    vagrant destroy -f
-    vagrant up
-    rm -f package.box || true
-    vagrant package
-    vagrant box add -f "jiro4989/$d" package.box
-    vagrant halt
-  )
-done
+(
+	cd base
+	vagrant destroy -f
+	vagrant up
+	rm -f package.box || true
+	vagrant package
+	vagrant box add -f "jiro4989/base-ubuntu-20.04" package.box
+	vagrant halt
+)
 
+(
+	cd base-gui
+	vagrant destroy -f
+	vagrant up
+	rm -f package.box || true
+	vagrant package
+	vagrant box add -f "jiro4989/base-gui-ubuntu-20.04" package.box
+	vagrant halt
+)
