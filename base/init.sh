e3a48c5c291ff6ec95be2d2c8e997c08738412a6
jiro4989 <jiroron666@gmail.com>
2020-11-07 18:39:29 +0900

    feat: remove monit

--- base/init.sh
+++ base/init.sh
@@ -56,7 +56,7 @@ curl https://nim-lang.org/choosenim/init.sh -sSf > /tmp/init.sh
 sh /tmp/init.sh -y >/dev/null 2>&1
 
 export PATH=\$PATH:\$HOME/.nimble/bin
-nimble install -Y inim bump subnet nimjson monit
+nimble install -Y inim bump subnet nimjson
 
 # dotfiles
 git clone https://github.com/jiro4989/dotfiles
