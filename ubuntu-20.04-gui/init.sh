69f9fabf8a66cd305a7b46c3abf77c7f072343fa
jiro4989 <jiroron666@gmail.com>
2020-12-23 00:36:26 +0900

    ubuntu-desktopは重いのでlxdeに変更

--- ubuntu-20.04-gui/init.sh
+++ ubuntu-20.04-gui/init.sh
@@ -3,7 +3,7 @@
 set -eu
 
 # GUI環境の構築
-apt-get install -y ubuntu-desktop xvfb
+apt-get install -y lxde xvfb
 
 # sdl2
 apt-get install -y libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-net-dev libsdl2-ttf-dev
