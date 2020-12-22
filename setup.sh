582b6e457f71e9024c3ddc0344983881cfcea4d2
jiro4989 <jiroron666@gmail.com>
2020-12-22 23:46:17 +0900

    スクリプトを修正

--- setup.sh
+++ setup.sh
@@ -2,22 +2,5 @@
 
 set -eu
 
-for d in base base-gui; do
-  (
-    cd "$d"
-    vagrant init
-    vagrant up
-    vagrant package
-    vagrant box add "jiro4989/$d" package.box
-    vagrant halt
-  )
-done
-
-for d in main main-gui; do
-  (
-    cd "$d"
-    vagrant init
-    vagrant up
-    vagrant halt
-  )
-done
+./build_box.sh base
+./save_snapshot.sh base
