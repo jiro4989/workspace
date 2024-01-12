94daa2f7c66292d74c46d2caebfe9eca53397343
jiro4989 <jiroron666@gmail.com>
2024-01-12 22:48:26 +0900

    fix: scoop を winget に変更

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,16 +1,6 @@
-iwr -useb get.scoop.sh | iex
-scoop bucket add extras
-
-scoop install teraterm
-scoop install slack
-scoop install 7zip
-scoop install vscode
-scoop install windows-terminal
-scoop install steam
-scoop install googlechrome
-scoop install vivaldi
-scoop install obsidian
-
-# Java
-scoop bucket add java
-scoop install openjdk17
+winget install Git.Git
+winget install Neovim.Neovim
+winget install Spotify.Spotify
+winget install Valve.Steam
+winget install SlackTechnologies.Slack
+winget install Microsoft.WindowsTerminal
