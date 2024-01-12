835a1fe9953d0f6c0a4859ab993ac5c6fab341c4
jiro4989 <jiroron666@gmail.com>
2024-01-12 22:50:02 +0900

    fix: VSCode を追加

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -4,3 +4,4 @@ winget install Spotify.Spotify
 winget install Valve.Steam
 winget install SlackTechnologies.Slack
 winget install Microsoft.WindowsTerminal
+winget install Microsoft.VisualStudioCode
