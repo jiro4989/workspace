0c798522f8f921c18de5dde836d9cfb2f93a0680
jiro4989 <jiroron666@gmail.com>
2026-05-27 01:08:18 +0900

    feat: flake.nix を追加

---
+++ wsl2_nix/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "開発環境";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    # 複数のシステム(Linux, Macなど)に簡単に対応するためのライブラリ
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        Cambrian = "test";
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.git
+          ];
+
+          shellHook = ''
+            echo "Nix のシェルに接続しました"
+          '';
+        };
+      }
+    );
+}
