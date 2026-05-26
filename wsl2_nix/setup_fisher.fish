2c0dd198d77e4cbc078026525521bb828a0163a6
jiro4989 <jiroron666@gmail.com>
2026-05-27 02:05:28 +0900

    add: setup_fisher.fish

---
+++ wsl2_nix/setup_fisher.fish
@@ -0,0 +1,6 @@
+#!/usr/bin/env fish
+
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/791da644d33d392216f6b1a9b5fc1e470db6d7f2/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+fisher install edc/bass
+fisher install oh-my-fish/theme-clearance
+fisher install fisherman/z
